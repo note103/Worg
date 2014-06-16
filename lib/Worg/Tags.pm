a65dc29985a858a6f9277664b8a73ad9a98e71d5
Hiroaki Kadomatsu <note103@gmail.com>
2014-06-16 22:16:34 +0900

    Modify typo

--- lib/Worg/Tags.pm
+++ lib/Worg/Tags.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Tags {
     sub tags {
-        return 'amazon||books|buy|coffee|docker|drink|eat|english|floss|git|housework|junkudo|lunch|mail|maintenance|memo|mojolicious|money|must|mysql|nail|perl|postgres|programming|rails|reading|rest|ruby|twitter|vagrant|vim|wake|wash|wish|youtube';
+        return 'amazon|books|buy|coffee|docker|drink|eat|english|floss|git|housework|junkudo|lunch|mail|maintenance|memo|mojolicious|money|must|mysql|nail|perl|postgres|programming|rails|reading|rest|ruby|twitter|vagrant|vim|wake|wash|wish|youtube';
     }
     sub change {
         return {
