5c17b47368a0f1ef5b0de606723e3712fe46e5b7
Hiroaki Kadomatsu <note103@gmail.com>
2014-12-10 01:29:21 +0900

    Update for Advent Calendar

--- lib/Worg/Tags.pm
+++ lib/Worg/Tags.pm
@@ -4,10 +4,10 @@ use warnings;
 
 package Tags {
     sub tags {
-        return 'source|食器|掃除|洗|メール|amazon|books|buy|coffee|docker|drink|eat|english|git|lunch|mail|mojolicious|money|mysql|nail|papix|perlentrance|perl|postgres|programming|rails|ruby|twitter|vagrant|vim|wash|youtube';
+        return 'source|食器|掃除|洗|メール|山本|yamamoto|完了|done|amazon|books|buy|coffee|docker|drink|eat|english|git|lunch|mail|mojolicious|money|mysql|nail|perlentrance|perl|postgres|programming|rails|ruby|twitter|vagrant|vim|wash|youtube';
     }
     sub change_tags {
-        return '|食器|掃除|洗|メール|papix|';
+        return '|食器|掃除|洗|メール|山本|完了|';
     }
     sub change_words {
         return {
@@ -15,7 +15,8 @@ package Tags {
             '掃除' => 'cleaning',
             '洗' => 'wash',
             'メール' => 'mail',
-            'papix' => 'perlentrance',
+            '山本' => 'yamamoto',
+            '完了' => 'done',
         };
     }
 }
